02f2637e46311fbab387b3bdafe62ce204a08e70
Winston Durand <me@winstondurand.com>
2014-10-06 20:21:00 -0700

    Added hello world program

---
+++ GitTest/src/gitTest/HelloWorld.java
@@ -0,0 +1,26 @@
+/**********************************
+ * File: HelloWorld.java
+ * 
+ * Author: Winston Durand
+ * Modified: Oct 6, 2014
+ **********************************/
+package gitTest;
+
+/**
+ * Hello world
+ * 
+ * @author Winston Durand
+ */
+public class HelloWorld {
+
+    /**
+     * Simple Hello world program
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+
+    }
+
+}
