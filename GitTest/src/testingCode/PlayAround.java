4301abad62c26cc8d6ddaeb9dfb2f751fc31026a
Winston Durand <me@winstondurand.com>
2014-10-12 16:10:58 -0700

    added gitignore files

--- GitTest/src/testingCode/PlayAround.java
+++ GitTest/src/testingCode/PlayAround.java
@@ -17,7 +17,7 @@ public class PlayAround {
      */
     public static void main(String[] args) {
         System.out.println("helell".split("(?:l)").length);
-        
+
         int count = 0;
         String str = "code co e  ";
         for (int i=0; i<str.length()-3; i++) {
@@ -26,8 +26,25 @@ public class PlayAround {
                 i += 3;
             }
         }
-        
+
         System.out.println(count);
+
+        int myVal = 2;
+        String myChoice = "bye";
+
+        if(myVal == 1) {
+            if (myChoice.equals("hi")) {
+                System.out.println ("John");
+            } else if (myChoice.equals("bye")) {
+                System.out.println ("Joe");
+            } else if(myVal == 2) {
+                if (myChoice.equals("hi")) {
+                    System.out.println ("Jesse");
+                } else if (myChoice.equals("bye")) {
+                    System.out.println ("Jordan");
+                }
+            }
+        }
 
     }
 
