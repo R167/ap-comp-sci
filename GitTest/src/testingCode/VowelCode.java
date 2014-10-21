d77d4073e8f3e815177b202bd0a347d24818995a
Winston Durand <winston.durand17@auhsdschools.org>
2014-10-21 14:05:39 -0700

    updated to handle y recursion

--- GitTest/src/testingCode/VowelCode.java
+++ GitTest/src/testingCode/VowelCode.java
@@ -13,12 +13,13 @@ import java.util.Scanner;
  * @author Winston Durand
  */
 public class VowelCode {
-    public static char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+    public static final char[] VOWELS = {'a', 'e', 'i', 'o', 'u'};
+    public static final int MAXYS = 3;
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String input = null;
-        int[] count = new int[vowels.length];
+        int[] count = new int[VOWELS.length];
         int y = 0;
         try {
             System.out.print("Enter a string to check for vowels: ");
@@ -28,19 +29,23 @@ public class VowelCode {
         }
 
         for (int i = 0; i < input.length(); i++) {
+            try {
             if (isVowel(input, i)) {
                 char cur = input.charAt(i);
                 if (cur != 'y') {
-                    count[inList(vowels, cur)] += 1;
+                    count[inList(VOWELS, cur)] += 1;
                 } else {
                     y += 1;
                 }
             }
+            } catch (java.io.IOException err) {
+                System.out.println(err.getMessage() + " Ignoring...");
+            }
         }
 
         int vowelCount = y;
-        for (int i=0; i<vowels.length; i++) {
-            System.out.printf("%s: %d\n", vowels[i], count[i]);
+        for (int i=0; i<VOWELS.length; i++) {
+            System.out.printf("%s: %d\n", VOWELS[i], count[i]);
             vowelCount += count[i];
         }
         System.out.println("y: " + y);
@@ -49,19 +54,20 @@ public class VowelCode {
         System.out.println("punctuation: " + (input.replaceAll("[a-z0-9\\s]", "").length()));
     }
 
-    public static boolean isVowel(String str, int index, int depth) {
+    public static boolean isVowel(String str, int index, int depth) throws java.io.IOException {
         if (index < str.length() && index >= 0) {
             char letter = str.charAt(index);
-            if (inList(vowels, letter) > -1) {
+            if (inList(VOWELS, letter) > -1) {
                 return true;
-            } else if (letter == 'y' && depth < 10) { // Prevent stack overflows
+            } else if (letter == 'y') {
+                if (depth > MAXYS - 2) throw new java.io.IOException("Too many 'y's. Char " + index); // avoid infinite recursion
                 return !isVowel(str, index + 1, depth + 1) && !isVowel(str, index - 1, depth + 1);
             }
         }
         return false;
     }
     
-    public static boolean isVowel(String str, int index) {
+    public static boolean isVowel(String str, int index) throws java.io.IOException {
         return isVowel(str, index, 0);
     }
 
