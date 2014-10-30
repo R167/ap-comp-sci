7cc2da05809d04f51b2a37501b820676bee157dd
Winston Durand <winston.durand17@auhsdschools.org>
2014-10-30 14:03:37 -0700

    added gitignore and fixed indentation

--- Lab8/src/Lab8Restaurant/MainSystem.java
+++ Lab8/src/Lab8Restaurant/MainSystem.java
@@ -2,12 +2,12 @@ package Lab8Restaurant;
 
 public class MainSystem {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
 
-		Restaurant myRestaurant = new Restaurant();
-		
-		myRestaurant.run();
-	}
+        Restaurant myRestaurant = new Restaurant();
+
+        myRestaurant.run();
+    }
 
 }
