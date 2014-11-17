10846b6b1edac12233690e236ee65fcb0ef11b6e
Winston Durand <winston.durand17@auhsdschools.org>
2014-11-17 14:11:14 -0800

    Wrote more code

---
+++ library/src/library/Book.java
@@ -0,0 +1,30 @@
+/**********************************
+ * File: Book.java
+ * 
+ * Author: Winston Durand
+ * Modified: Nov 17, 2014
+ **********************************/
+package library;
+
+/**
+ * @author Winston Durand
+ */
+public class Book {
+    private String author = null;
+    private String title = null;
+    
+    
+    
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
---
+++ library/src/library/Library.java
@@ -0,0 +1,23 @@
+/**********************************
+ * File: Library.java
+ * 
+ * Author: Winston Durand
+ * Modified: Nov 17, 2014
+ **********************************/
+package library;
+
+import java.util.HashMap;
+
+/**
+ * @author Winston Durand
+ */
+public class Library {
+    private String name = null;
+    private HashMap<String, Book> books = new HashMap<String, Book>();
+    
+    public Library(String libraryName) {
+        this.name = libraryName;
+    }
+    
+    public createBook(String bookTitle, String bookAuthor))
+}
