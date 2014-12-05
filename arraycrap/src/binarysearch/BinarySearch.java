4f13a40151b8db17c8d1007c400696384d46c9fc
Winston Durand <me@winstondurand.com>
2014-12-04 22:38:42 -0800

    wrote the binary search algorithm or something

--- arraycrap/src/binarysearch/BinarySearch.java
+++ arraycrap/src/binarysearch/BinarySearch.java
@@ -16,20 +16,41 @@ public class BinarySearch {
      */
     public static void main(String[] args) {
         int[] search = {1, 3, 5, 9, 12, 15, 18, 24};
+        int[] vals = {-3, 9, 12, 24, 52};
         
-
+        System.out.println("Array to search");
+        
+        for (int v : search) {
+            System.out.print(v + " ");
+        }
+        
+        System.out.println();
+        
+        for (int v : vals) {
+            System.out.println("\nSearching for " + v);
+            binarySearch(search, v);
+        }
     }
     
     public static int binarySearch(int[] arr, int val) {
         int min = 0;
         int max = arr.length - 1;        
-        while (true) {
-            
+        while (min <= max) {
+            int pivot = min + (max - min) / 2;
+            System.out.printf("Max: %d | Min: %d | Pivot: %d\n", max, min, pivot);
+            if (val < arr[pivot]) {
+                max = pivot - 1;
+                System.out.printf("%d is less than %d. Max is now %d\n\n", val, arr[pivot], max);
+            } else if (val > arr[pivot]) {
+                min = pivot + 1;
+                System.out.printf("%d is less than %d. Min is now %d\n\n", val, arr[pivot], min);
+            } else {
+                System.out.printf("%d is at index %d\n\n", val, pivot);
+                return pivot;
+            }
         }
+        System.out.println("Min is no longer less than or equal to max. Exit while loop.");
+        System.out.printf("%d is not in the array\n\n", val);
+        return -1;
     }
-    
-    public static int middle(int min, int max) {
-        return (max - min) / 2;
-    }
-
 }
