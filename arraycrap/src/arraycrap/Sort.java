e0b4a6c2fe2be1c5b4d9b31d608107ed772a110d
Winston Durand <winston.durand17@auhsdschools.org>
2014-12-16 14:10:08 -0800

    wrote code,,,

--- arraycrap/src/arraycrap/Sort.java
+++ arraycrap/src/arraycrap/Sort.java
@@ -16,28 +16,49 @@ public class Sort {
      */
     public static void main(String[] args) {
         int[] nums = {7,0,-6,4};
-        
-        selectionSort(nums);
-        
-        ArrayIndeces.printArr(nums);
+
+        int[] selection = copy(nums);
+        int[] insertion = copy(nums);
+
+        selectionSort(selection);
+        ArrayIndeces.printArr(selection);
+
+        insertionSort(insertion);
+        ArrayIndeces.printArr(insertion);
 
     }
 
     public static void selectionSort(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
-            int biggest = i;
+            int smallest = i;
             for (int j = i + 1; j < nums.length; j++) {
-                if (nums[j] > nums[biggest]) biggest = j;
+                if (nums[j] < nums[smallest]) smallest = j;
             }
-            swap(nums, i, biggest);
+            swap(nums, i, smallest);
         }
-        
+
     }
-    
+
+    public static void insertionSort(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            for (int k = i; k > 0 && nums[k] < nums[k-1]; k--) {
+                swap(nums, k, k - 1);
+            }
+        }
+    }
+
     public static void swap(int[] nums, int i1, int i2) {
         int tmp = nums[i1];
         nums[i1] = nums[i2];
         nums[i2] = tmp;
+    }
+
+    public static int[] copy(int[] nums) {
+        int[] ret = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            ret[i] = nums[i];
+        }
+        return ret;
     }
 
 }
