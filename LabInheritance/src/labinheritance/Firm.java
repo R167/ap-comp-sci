87dc1ae90b4e42541891f2da97bd3fae045e2c23
Winston Durand <me@winstondurand.com>
2015-02-22 14:42:32 -0800

    Copied code and did assignment

---
+++ LabInheritance/src/labinheritance/Firm.java
@@ -0,0 +1,13 @@
+/********************
+ * File: Firm.java
+ * Author: Winston Durand
+ * Modified: Feb 22, 2015
+ ********************/
+package labinheritance;
+
+public class Firm {
+    public static void main(String[] args) {
+        Staff personnel = new Staff();
+        personnel.payday();
+    }
+}
